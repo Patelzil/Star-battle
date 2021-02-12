f672d81156f7909c2abdabab524f1f4a8581f6d2
Patelzil <58107277+Patelzil@users.noreply.github.com>
2021-02-12 02:45:59 -0600

    Consistency of variables checked

--- backtrack.py
+++ backtrack.py
@@ -1,14 +1,53 @@
 class CSP:
-    def __init__(self, variables, domains):
+    def __init__(self, variables, domains,size):
         self.variables = variables  # variables to be constrained
         self.domains = domains  # domain of each variable
+        self.size = size  # domain of each variable
         self.constraints = {}
         self.count = 0
 
-    def consistent(self, var, assignment):
-        return True
+    # need to pass variable for block check
+    def consistent(self, value, assignment):
+        result = True
+        # Check if any variable in the assignment has the same value
+        for key in assignment:
+            if assignment[key] == value:
+                return False
 
-    def select_variable(self,assignment,heuristic):
+        # Check if the value is directly neighbouring another variable
+        for key in assignment:
+            # value is cell number
+            # check left cell
+            if (assignment[key] == value-1) and (value-1 % self.size != 0):
+                return False
+            # check right cell
+            if (assignment[key] == value+1) and (value+1 % self.size != 1):
+                return False
+            # check top cell
+            if (assignment[key] == value-self.size) or (value-self.size <= 0):
+                return False
+            # check top left cell
+            if (assignment[key] == value-self.size - 1) or (value-self.size <= 0):
+                return False
+            # check top right cell
+            if (assignment[key] == value-self.size + 1) or (value-self.size <= 0):
+                return False
+
+            # check bottom cell
+            if (assignment[key] == value+self.size) or (value+self.size > self.size*self.size) :
+                return False
+            # check bottom left
+            if (assignment[key] == value+self.size-1) or (value+self.size > self.size*self.size) :
+                return False
+            # check bottom right
+            if (assignment[key] == value+self.size+1) or (value+self.size > self.size*self.size) :
+                return False
+
+
+
+        return result
+
+    def select_variable(self, assignment, heuristic):
         if heuristic == "most_constrained":
             return self.most_constrained(assignment)
         elif heuristic == "most_constraining":
@@ -16,18 +55,21 @@ class CSP:
         elif heuristic == "hybrid":
             return self.hybrid(assignment)
 
-    def most_constrained(self,assignment):
-        var = self.variables[self.count]
-        self.count += 1
+    def most_constrained(self, assignment):
+        var = -1
+        for var in self.variables:
+            if var not in assignment:
+                return var
+
         return var
 
-    def most_constraining(self,assignment):
+    def most_constraining(self, assignment):
         pass
 
-    def hybrid(self,assignment):
+    def hybrid(self, assignment):
         pass
 
-    def backtracking(self, assignment,heuristic):
+    def backtracking(self, assignment, heuristic):
         result = {}
         # base case
         if len(assignment) == len(self.variables):
@@ -35,8 +77,11 @@ class CSP:
         else:
             # assignment is a dict
             # domain is a dict
-            variable = self.select_variable(assignment,heuristic)
+            variable = self.select_variable(assignment, heuristic)
             # d[v] //edit so FC
+            if variable == -1:
+                return assignment
+
             for value in self.domains[variable]:
                 if self.consistent(value, assignment):
                     assignment[variable] = value
@@ -70,12 +115,14 @@ def read_file():
         domains[variables[count]] = domain
         domains[variables[count+1]] = domain
         count += 2
-
-    return variables, domains
+    size = int(count/2)
+    return variables, domains, size
 
 def main():
-    variables, domains = read_file()
-    csp = CSP(variables,domains)
+    variables, domains,size = read_file()
+    csp = CSP(variables, domains, size)
     resyi = csp.backtracking({}, "most_constrained")
     print(resyi)
+
+
 main()
