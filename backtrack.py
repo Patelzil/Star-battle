4ab73c03d9db2a8d57fe9ca498a288debeb529f2
Patelzil <58107277+Patelzil@users.noreply.github.com>
2021-02-15 02:53:30 -0600

    added most-contrained heuristic

--- backtrack.py
+++ backtrack.py
@@ -1,49 +1,87 @@
+import copy
+
 class CSP:
-    def __init__(self, variables, domains,size):
+    def __init__(self, variables, domains, size):
         self.variables = variables  # variables to be constrained
         self.domains = domains  # domain of each variable
         self.size = size  # domain of each variable
         self.constraints = {}
         self.count = 0
 
+    def check_neighbours(self, assignment, value):
+        # Check if the value is directly neighbouring another variable
+        for key in assignment:
+            # value is cell number
+            # check left cell
+            if (assignment[key] == value - 1) and (value - 1 % self.size != 0):
+                return False
+            # check right cell
+            if (assignment[key] == value + 1) and (value + 1 % self.size != 1):
+                return False
+            # check top cell
+            if (assignment[key] == value - self.size) or (value - self.size <= 0):
+                return False
+            # check top left cell
+            if (assignment[key] == value - self.size - 1) or (value - self.size <= 0):
+                return False
+            # check top right cell
+            if (assignment[key] == value - self.size + 1) or (value - self.size <= 0):
+                return False
+
+            # check bottom cell
+            if (assignment[key] == value + self.size) or (value + self.size > self.size * self.size):
+                return False
+            # check bottom left
+            if (assignment[key] == value + self.size - 1) or (value + self.size > self.size * self.size):
+                return False
+            # check bottom right
+            if (assignment[key] == value + self.size + 1) or (value + self.size > self.size * self.size):
+                return False
+
+
+        return True
+
+    def check_column(self, assignment, value):
+        count = 0
+
+        for key in assignment:
+            if (assignment[key] % self.size) == (value % self.size):
+                count += 1
+            if count == 2:
+                return False
+        return True
+
+    def check_row(self, assignment, value):
+        count = 0
+        for key in assignment:
+            if int((assignment[key] - 1) / self.size) == int((value - 1) / self.size):
+                count += 1
+            if count == 2:
+                return False
+        return True
+
     # need to pass variable for block check
-    def consistent(self, value, assignment):
+    def consistent(self, value, variable, assignment):
         result = True
         # Check if any variable in the assignment has the same value
         for key in assignment:
             if assignment[key] == value:
                 return False
 
-        # Check if the value is directly neighbouring another variable
-        for key in assignment:
-            # value is cell number
-            # check left cell
-            if (assignment[key] == value-1) and (value-1 % self.size != 0):
-                return False
-            # check right cell
-            if (assignment[key] == value+1) and (value+1 % self.size != 1):
-                return False
-            # check top cell
-            if (assignment[key] == value-self.size) or (value-self.size <= 0):
-                return False
-            # check top left cell
-            if (assignment[key] == value-self.size - 1) or (value-self.size <= 0):
-                return False
-            # check top right cell
-            if (assignment[key] == value-self.size + 1) or (value-self.size <= 0):
-                return False
+        neighbours_valid = self.check_neighbours(assignment, value)
 
-            # check bottom cell
-            if (assignment[key] == value+self.size) or (value+self.size > self.size*self.size) :
-                return False
-            # check bottom left
-            if (assignment[key] == value+self.size-1) or (value+self.size > self.size*self.size) :
-                return False
-            # check bottom right
-            if (assignment[key] == value+self.size+1) or (value+self.size > self.size*self.size) :
-                return False
+        if not neighbours_valid:
+            return False
 
+        column_valid = self.check_column(assignment, value)
+        # check column
+        if not column_valid:
+            return False
 
+        row_valid = self.check_row(assignment, value)
+        # check column
+        if not row_valid:
+            return False
 
         return result
 
@@ -56,12 +94,26 @@ class CSP:
             return self.hybrid(assignment)
 
     def most_constrained(self, assignment):
-        var = -1
+        count = 2147483647
+        most_constrained = -1
+
+        reduced_domains = copy.deepcopy(self.domains)
+
         for var in self.variables:
             if var not in assignment:
-                return var
+                for key in assignment:
+                    assigned_value = assignment[key]  # variable
+                    if assigned_value in reduced_domains[var]:
+                        reduced_domains[var].remove(assigned_value)
 
-        return var
+        for key in reduced_domains:
+            if key not in assignment:
+                if len(reduced_domains[key]) < count:
+                    count = len(reduced_domains[key])
+                    most_constrained = key
+
+
+        return most_constrained
 
     def most_constraining(self, assignment):
         pass
@@ -83,26 +135,26 @@ class CSP:
                 return assignment
 
             for value in self.domains[variable]:
-                if self.consistent(value, assignment):
+                if self.consistent(value, variable, assignment):
                     assignment[variable] = value
                     result = self.backtracking(assignment, heuristic)
 
                     if result is not None:
                         return assignment
-
                     else:
                         assignment.pop(variable)
 
-            return None
+            return assignment
 
         return result
+
 
 # Returns a list of variables, and domains.
 def read_file():
     file = open("input")
     lines = file.readlines()
     # List of variables.
-    variables = [x+1 for x in range(len(lines)*2)]
+    variables = [x + 1 for x in range(len(lines) * 2)]
     # Dictionary of domains
     domains = {}
     # Keeping track of Variables
@@ -113,16 +165,21 @@ def read_file():
         for value in str_values:
             domain.append(int(value))
         domains[variables[count]] = domain
-        domains[variables[count+1]] = domain
+        domains[variables[count + 1]] = domain
         count += 2
-    size = int(count/2)
+    size = int(count / 2)
     return variables, domains, size
 
+
 def main():
-    variables, domains,size = read_file()
+    variables, domains, size = read_file()
     csp = CSP(variables, domains, size)
     resyi = csp.backtracking({}, "most_constrained")
     print(resyi)
 
+
+    hars = {"z":[1,2]}
+    if "h" not in hars:
+        print("not in")
 
 main()
